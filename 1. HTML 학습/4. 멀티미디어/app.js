b46fdf79941e11b46e48df73842c65b64ea48e89
pfull-coder <himcha7@naver.com>
2020-12-22 00:35:23 +0900

    html, css 학습완료

---
+++ 1. HTML 학습/4. 멀티미디어/app.js
@@ -0,0 +1,6 @@
+const changeBtn = document.getElementById('change-btn');
+const pet = document.getElementById('my-pet');
+
+changeBtn.addEventListener('click', function() {
+    pet.innerText = "사실은 바보야 ㅋㅋㅋ";
+});
